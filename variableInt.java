1ff7bf7de7495cf9f754b1e002069962937d8c76
vivianagonzalez49 <87345233+vivianagonzalez49@users.noreply.github.com>
2021-10-01 01:48:46 -0500

    Exercise section 2-7

---
+++ variableInt.java
@@ -0,0 +1,13 @@
+/*
+Sección 2. Fundamentos Java: Variables en Java
+7. Variables en Java
+ */
+public class Variables {
+    public static void main(String args[]){ //este método permite ejecutar el programa
+        /*Palabra reservada int para entero, seguido del identificador (nombre de variable), luego
+        operador de asignación y por último el valor  
+        */
+        int miVariableEntera = 10; //Se declara la variable
+        System.out.println(miVariableEntera); //Imprimir variable en consola
+    }
+}
