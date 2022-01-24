5cc136dacb42acfc48c4248e635fb3cfa166ba80
vivianagonzalez49 <87345233+vivianagonzalez49@users.noreply.github.com>
2022-01-23 23:39:58 -0500

    DB connection - MySql

---
+++ LaboratorioJDBC/src/main/java/test/TestManejoUsuarios.java
@@ -0,0 +1,49 @@
+package test;
+
+import datos.UsuarioDAO;
+import domain.Usuario;
+import java.util.List;
+
+/**
+ *
+ * @author vafla
+ */
+public class TestManejoUsuarios {
+    public static void main(String[] args) {
+        
+        UsuarioDAO usuarioDao = new UsuarioDAO();
+        //Aqui se mostraria la informacion del metodo seleccionar. Fue un ejemplo, luego se usara en la otra parte del codigo
+//        List<Usuario> usuarios = usuarioDao.seleccionar();
+//        for(Usuario username: usuarios){
+//            System.out.println("usuario = " + username);
+//        }
+//        usuarios.forEach(username -> {
+//            System.out.println("username = " + username);
+//        });
+
+
+        //Insertar un nuevo objeto de tipo Usuario
+//        Usuario usuarioNuevo = new Usuario("usertest", "userpass");
+//        usuarioDao.insertar(usuarioNuevo);
+
+        
+        //Modificar un objeto de Usuario existente (registro)
+//        Usuario usuarioModificar = new Usuario(1, "vgonzalez", "vgonZalez");
+//        usuarioDao.actualizar(usuarioModificar);
+        
+
+        //Eliminar un objeto de Usuario existente
+//        Usuario usuarioEliminar = new Usuario(3);
+//        usuarioDao.eliminar(usuarioEliminar);
+
+        //otra forma eliminar. Todo en una sola linea
+//        usuarioDao.eliminar(new Usuario(3));
+        
+        
+        List<Usuario> usuarios = usuarioDao.seleccionar();
+        for(Usuario username: usuarios){
+            System.out.println("usuario = " + username);
+        }
+        
+    }
+}
